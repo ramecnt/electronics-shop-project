53ae2e6ff4819f3138dd791f762c66ebc9f60c9d
nikolay <n.ilin@it-park.tech>
2023-09-25 11:05:27 +0300

    hw1

--- src/item.py
+++ src/item.py
@@ -13,7 +13,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self._append()
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +24,16 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+
+    def _append(self) -> None:
+        """
+        Скрытый метод добавляющиий экземпляр класса в атрибут all.
+        """
+        self.all.append(self)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+from src.item import Item
+
+
+test_item = Item('Test', 12000, 5)
+test_item.pay_rate = 1.2
+
+
+def test_calculate_total_price():
+    assert test_item.price * test_item.quantity == test_item.calculate_total_price()
+
+
+def test_apply_discount():
+    usuall_price = test_item.price * test_item.pay_rate
+    test_item.apply_discount()
+    price = test_item.price
+    assert usuall_price == price
