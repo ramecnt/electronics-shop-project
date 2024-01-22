f521833005778b884ad8830a1f1258c9cdcb3235
nikolay <n.ilin@it-park.tech>
2024-01-22 10:29:44 +0300

    hw 4 updated

--- src/phone.py
+++ src/phone.py
@@ -2,8 +2,16 @@ from src.item import Item
 
 
 class Phone(Item):
-    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int = 1) -> None:
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self._number_of_sim = number_of_sim
 
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
 
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim: int) -> None:
+        if number_of_sim <= 0:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self._number_of_sim = number_of_sim
